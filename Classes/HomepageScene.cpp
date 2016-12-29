29a01bbb6742d9f8676499a054cb5064d2606213
MonsterMasterAF <andrewbf@lenovo.com>
2016-12-29 12:43:24 +0800

    updated

--- Classes/HomepageScene.cpp
+++ Classes/HomepageScene.cpp
@@ -49,12 +49,12 @@ bool Homepage::init()
     // create menu, it's an autorelease object
     Vector<MenuItem*> MenuItems;
     
-    auto backgroundMusicItem = MenuItemImage::create("music_on.jpg", "music_off.jpg", CC_CALLBACK_1(Homepage::controlMusicCallback, this));
+    auto backgroundMusicItem = MenuItemImage::create("res/music_on.jpg", "res/music_off.jpg", CC_CALLBACK_1(Homepage::controlMusicCallback, this));
     //backgroundMusicItem->setAnchorPoint(Vec2(0,0));
     backgroundMusicItem->setScale(0.5);
     backgroundMusicItem->setPosition(Vec2(origin.x + visibleSize.width - backgroundMusicItem->getContentSize().width/2 ,
                                           origin.y + backgroundMusicItem->getContentSize().height/2));
-    auto backgroundSoundEffectItem = MenuItemImage::create("play_music.png", "play_music.png", "stop_music.png", CC_CALLBACK_1(Homepage::controlMusicCallback, this));
+    auto backgroundSoundEffectItem = MenuItemImage::create("res/play_music.png", "res/play_music.png", "stop_music.png", CC_CALLBACK_1(Homepage::controlMusicCallback, this));
     backgroundSoundEffectItem->setScale(0.5);
     backgroundSoundEffectItem->setPosition(Vec2(origin.x+visibleSize.width-backgroundMusicItem->getContentSize().width-backgroundSoundEffectItem->getContentSize().width/2, origin.y+backgroundSoundEffectItem->getContentSize().height/2));
     MenuItems.pushBack(backgroundMusicItem);
