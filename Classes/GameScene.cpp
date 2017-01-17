641a195e493a87989f719a891d0a70e501544385
MonsterMasterAF <andrewbf@lenovo.com>
2017-01-17 12:47:10 +0800

    check

---
+++ Classes/GameScene.cpp
@@ -0,0 +1,9 @@
+//
+//  GameScene.cpp
+//  pionner
+//
+//  Created by Andrew on 2017/1/9.
+//
+//
+
+#include "GameScene.hpp"
---
+++ Classes/GameScene.h
@@ -0,0 +1,14 @@
+//
+//  GameScene.hpp
+//  pionner
+//
+//  Created by Andrew on 2017/1/9.
+//
+//
+
+#ifndef GameScene_hpp
+#define GameScene_hpp
+
+#include <stdio.h>
+
+#endif /* GameScene_hpp */
